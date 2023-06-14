f2b0881dc745f7ba33f900fe4046969b04f70f17
Kirill Snopko <kirillsnopko@gmail.com>
2023-06-14 16:54:38 +0300

    update subject component and add delete component with modal

--- src/app/layouts/journal/subject/subject-list/subject-list.component.ts
+++ src/app/layouts/journal/subject/subject-list/subject-list.component.ts
@@ -1,38 +1,7 @@
-import { Component, OnInit, Type, Input } from '@angular/core';
-import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpSubjectProviderService } from 'src/app/http/provider/http-subject-provider.service';
-
-@Component({
-  selector: 'ng-modal-confirm',
-  standalone: true,
-  template: `
-  <div class="modal-header">
-    <h5 class="modal-title" id="modal-basic-title">Delete Confirmation</h5>
-    <button type="button" class="close" aria-label="Close" (click)="modal.dismiss('Cancel')">
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
-  <div class="modal-body">
-    <p>Are you sure you want to delete?</p>
-  </div>
-  <div class="modal-footer">
-    <button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('Cancel')">CANCEL</button>
-    <button type="button" ngbAutofocus class="btn btn-success" (click)="modal.close('Ok')">OK</button>
-  </div>
-  `,
-})
-
-export class NgModalConfirm {
-  constructor(public modal: NgbActiveModal) { }
-}
-
-const MODALS: { [name: string]: Type<any> } = {
-  deleteModal: NgModalConfirm,
-};
-
-
+import { SubjectDeleteComponent } from '../subject-delete/subject-delete.component';
 
 
 @Component({
@@ -42,7 +11,7 @@ const MODALS: { [name: string]: Type<any> } = {
 })
 export class SubjectListComponent implements OnInit {
   subjects: any = [];
-  constructor(private router: Router, private subjectProvider: HttpSubjectProviderService, private modalService: NgbModal, private toastr: ToastrService) { }
+  constructor(private router: Router, private subjectProvider: HttpSubjectProviderService, private delComp: SubjectDeleteComponent) { }
 
   ngOnInit(): void {
     this.getSubjects();
@@ -73,41 +42,10 @@ export class SubjectListComponent implements OnInit {
   }
 
   deleteSubjectConfirmation(subject: any) {
-    this.modalService.open(MODALS['deleteModal'],
-      {
-        ariaLabelledBy: 'modal-basic-title'
-      }).result.then((result) => {
-        this.deleteSubject(subject);
-      },
-        (reason) => { });
+    this.delComp.deleteConfirmation(subject, this);
   }
 
-  /*
-    delete() {
-      this.fruitService.delete(this.idTodelete).subscribe({
-        next: (data) => {
-          this.allFruits = this.allFruits.filter(_ => _.id != this.idTodelete)
-          this.deleteModal.hide();
-        },
-      });
-    }
-  */
-
-
-
-  deleteSubject(subject: any) {
-
-    this.subjectProvider.delete(subject.id).subscribe((data: any) => {
-      /* if (data != null && data.body != null) {
-         var resultData = data.body;
-         //if (resultData != null && resultData.isSuccess) {
-           this.toastr.success(resultData.message);
-           this.getSubjects();
-         //}
-       }*/
-      this.toastr.success("Успешно");
-      this.getSubjects();
-    },
-      (error: any) => { });
+  getProvider(){
+    return this.subjectProvider;
   }
 }
