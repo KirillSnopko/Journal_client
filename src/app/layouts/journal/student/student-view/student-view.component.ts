650e548238f44984d3057309debac445208e0cfe
Kirill Snopko <kirillsnopko@gmail.com>
2023-07-12 16:45:28 +0300

    deleted unused

--- src/app/layouts/journal/student/student-view/student-view.component.ts
+++ src/app/layouts/journal/student/student-view/student-view.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { MatDialog } from '@angular/material/dialog';
@@ -9,7 +9,6 @@ import { ApiRoutes } from 'src/app/http/api-routes';
 import { HttpProviderService } from 'src/app/http/provider/http-provider.service';
 import { Profile } from 'src/app/models/student/profile';
 import { ProfileUpdate } from 'src/app/models/student/profile-update';
-import { CourseCreate } from 'src/app/models/course/course-create';
 import { CourseCreateDialogComponent } from '../../course/course-create-dialog/course-create-dialog.component';
 
 
@@ -24,26 +23,9 @@ export class StudentViewComponent implements OnInit {
   profile: Profile = new Profile();
   upForm: any;
 
-
-  formats: string[] = ["online", "offline"];
-  subjects: any;
-  gradelevels: any;
-  firstFormGroup = this.fb.group({
-    format: ['', Validators.required],
-  });
-  secondFormGroup = this.fb.group({
-    subject: ['', Validators.required],
-  });
-  thirdFormGroup = this.fb.group({
-    gradelevel: ['', Validators.required],
-  });
-
-
-
   constructor(private route: ActivatedRoute,
     private provider: HttpProviderService,
     private toastr: ToastrService,
-    private router: Router,
     private fb: FormBuilder,
     public dialog: MatDialog,
   ) { }
@@ -58,8 +40,6 @@ export class StudentViewComponent implements OnInit {
       parentName: [this.profile.parentName],
       parentMobile: [this.profile.parentMobile]
     })
-
-    
   }
 
   getProfile() {
@@ -99,70 +79,6 @@ export class StudentViewComponent implements OnInit {
       },
         async error => {
           this.toastr.error(error.error.message);
-        });
-  }
-
-
-  getSubjects(stepper: any) {
-    this.provider.setUrl(ApiRoutes.subject).getList().subscribe((data: any) => {
-      if (data != null && data.body != null) {
-        this.subjects = data.body;
-        stepper.next();
-      }
-    },
-      (error: any) => {
-        if (error) {
-          if (error.status == 404) {
-            if (error.error && error.error.message) {
-              this.subjects = [];
-            }
-          }
-          this.toastr.error(error.error.message);
-        }
-      });
-  }
-
-  getGradeLevel(stepper: any) {
-    var id = this.secondFormGroup.value.subject!;
-    this.provider.setUrl(ApiRoutes.subject.toString() + id + ApiRoutes.grades.toString()).getList().subscribe((data: any) => {
-      if (data != null && data.body != null) {
-        this.gradelevels = data.body;
-        stepper.next();
-      }
-    },
-      (error: any) => {
-        if (error) {
-          if (error.status == 404) {
-            if (error.error && error.error.message) {
-              this.gradelevels = [];
-            }
-          }
-          this.toastr.error(error.error.message);
-        }
-      });
-  }
-
-  createCourse(stepper: any) {
-    var dto: CourseCreate = new CourseCreate();
-    dto.studentProfileId = this.profile.id;
-    dto.gradelevelId = this.thirdFormGroup.value.gradelevel!;
-    dto.type = this.firstFormGroup.value.format!;
-
-    this.provider.setUrl(ApiRoutes.course.toString())
-      .add(dto)
-      .subscribe(async data => {
-        if (data.status == 201) {
-          this.toastr.success("Добавлено!");
-          setTimeout(() => {
-
-            stepper.reset();
-            this.getProfile();
-
-          }, 500);
-        }
-      },
-        async error => {
-          this.toastr.error(error.error.errors.toString());
         });
   }
 
